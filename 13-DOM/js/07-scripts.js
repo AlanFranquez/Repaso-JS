748c259cac761c1323f86131fee8b134796f28f9
Mona Lisa <santifranquez@gmail.com>
2022-04-01 20:02:57 -0300

    new

--- 13-DOM/js/07-scripts.js
+++ 13-DOM/js/07-scripts.js
@@ -9,4 +9,17 @@ h1.style.color = 'red'
 // Ahora como cambiariamos la fuente?
 h1.style.fontFamily = 'Arial'
 
+// Usar esta sintaxis está bien, pero lo más recomnedable es agregar los diferente estilos en css
+// Y en javascript agregar o quitar clases según el momento
+// Como se hace esto?¿
+
+h1.classList.add('nueva-clase', 'otra-clases')
+
+// Ahora para quitar se hace de la siguiente forma
+
+const main = document.querySelector('main');
+
+console.log(main)
+main.classList.remove('contenido')
+
 console.log(h1)
---
+++ 13-DOM/js/08-scripts.js
@@ -0,0 +1,19 @@
+// Traversing 
+// Children -- > Para acceder a las hijos 
+// FirstElementChild 
+// LastElementChild
+// ParenElement -- > Para acceder a los padres
+// NexElementSiblings --> Acceder al siguiente elemento hermano
+// previousElementSiblings -- > Acceder al hermano anterior
+
+
+
+
+
+const card = document.querySelector('.card');
+
+
+
+console.log(card.children[1].children[1].previousElementSibling)
+
+

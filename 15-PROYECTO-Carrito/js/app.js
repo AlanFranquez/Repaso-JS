7c227136cc70493143ea9114ad83960459033f53
Mona Lisa <santifranquez@gmail.com>
2022-04-07 19:41:16 -0300

    nuevos pasos en el carrito de compras

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -13,8 +13,23 @@ function agregarCarrito(e) {
     e.preventDefault();
 
     if(e.target.classList.contains('agregar-carrito')) {
-        console.log('Agregaste al carrito')
+        leerCursos(e);
     }
 
     // console.log(e.target.classList)
+}
+
+function leerCursos(e) {
+    const cursos = e.target.parentElement.parentElement;
+    // console.log(cursos)
+
+    const infoCursos = {
+        imagen: cursos.querySelector('img').src,
+        titulo: cursos.querySelector('h4').textContent,
+        precio: cursos.querySelector('.precio span').textContent,
+        id: cursos.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+    }
+
+    console.log(infoCursos)
 }

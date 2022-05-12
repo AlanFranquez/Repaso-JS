01d336937dd3f83ab79786d3407d86b63cd8d7d4
Mona Lisa <santifranquez@gmail.com>
2022-05-11 21:27:11 -0300

    Avanzando con el proyecto de gasto semanal

--- 25-PROYECTO-GastoSemanal/js/app.js
+++ 25-PROYECTO-GastoSemanal/js/app.js
@@ -8,7 +8,7 @@ const form = document.querySelector('#agregar-gasto');
 eventListeners();
 function eventListeners() {
 
-    document.addEventListener('DOMContentLoaded', preguntarPresupuesto);
+    // document.addEventListener('DOMContentLoaded', preguntarPresupuesto);
 
     form.addEventListener('submit', agregarGasto)
 
@@ -19,7 +19,14 @@ function eventListeners() {
 class Presupuesto {
     constructor(presupuesto) {
         this.presupuesto = Number(presupuesto),
-        this.restante = Number(presupuesto)
+        this.restante = Number(presupuesto),
+        this.gastos = []
+    }
+
+    nuevoGasto(gasto) {
+        this.gastos = [...this.gastos, gasto]
+
+        console.log(this.gastos)
     }
 }
 
@@ -38,6 +45,29 @@ class UI {
 
     imprimirAlerta(mensaje, tipo) {
         const div = document.createElement('DIV');
+
+        div.textContent = mensaje;
+        div.classList.add('text-center', 'alert', 'mt-2', 'norepetir')
+
+        if(tipo === 'error') {
+            div.classList.add('alert-danger')
+        } else {
+            div.classList.add('alert-success')
+        }
+
+        
+        const h2 = document.querySelector('h2');
+
+        const norepetir = document.querySelectorAll('.norepetir');
+        if(norepetir.length === 0) {
+            h2.appendChild(div)
+
+            setTimeout(() => {
+                div.remove()
+            }, 2000);
+        }
+        
+
     }
 }
 
@@ -49,8 +79,6 @@ let presupuestoPedido;
 
 
 // FUNCIONES
-
-function preguntarPresupuesto() {
     const pedirPresupuesto = Number(prompt('¿Cuál es tu prespuesto?'));
 
     // console.log(pedirPresupuesto)
@@ -67,18 +95,39 @@ function preguntarPresupuesto() {
     // Para hacer que se vea en el html llamamos a la clase de UI junto a su metodo
     ui.insertarPresupuesto(presupuestoPedido) //presupuestopedido ya que ahí está la cantidad que se pidió;
 
+    
 
-}
 
 function agregarGasto(e) {
+
     e.preventDefault();
 
-    const gasto = document.querySelector('#gasto').value;
+    const nombre = document.querySelector('#gasto').value;
     const cantidad = document.querySelector('#cantidad').value;
 
-    if(gasto === '' || cantidad === '') {
+    if(nombre === '' || cantidad === '') {
         ui.imprimirAlerta('Ambos campos son obligatorios', 'error')
+
+
+        return;
+    } else if(cantidad <= 0 || isNaN(cantidad)) {
+        ui.imprimirAlerta('Cantidad no valida', 'error')
+
+        return
+    } 
+
+
+    // console.log('Añadiendo gastos')
+
+    const gasto = {
+        nombre: nombre,
+        cantidad: cantidad,
+        id: Date.now()
     }
+
+    presupuestoPedido.nuevoGasto(gasto)
+
+    
 }
 
 

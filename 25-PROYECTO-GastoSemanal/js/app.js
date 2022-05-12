656818138f6aa95e3bcb311cbb3541f1e5dd5ed3
Mona Lisa <santifranquez@gmail.com>
2022-05-12 19:51:49 -0300

    Guardando versión

--- 25-PROYECTO-GastoSemanal/js/app.js
+++ 25-PROYECTO-GastoSemanal/js/app.js
@@ -1,133 +1,66 @@
-//Variables
-const listado = document.querySelector('#gastos ul');
+// variables
 const form = document.querySelector('#agregar-gasto');
+const gastoListado = document.querySelector('#gastos ul');
 
-
-
-// EVENTOS
-eventListeners();
+// Eventos
+eventListeners()
 function eventListeners() {
+    document.addEventListener('DOMContentLoaded', preguntarPresupuesto);
 
-    // document.addEventListener('DOMContentLoaded', preguntarPresupuesto);
+    form.addEventListener('click', leerForm)
+}
 
-    form.addEventListener('submit', agregarGasto)
-
-};
-
-// CLASES
-
+// Clases
 class Presupuesto {
     constructor(presupuesto) {
-        this.presupuesto = Number(presupuesto),
-        this.restante = Number(presupuesto),
+        this.presupuesto = Number(presupuesto);
+        this.restante = Number(presupuesto);
         this.gastos = []
     }
-
-    nuevoGasto(gasto) {
-        this.gastos = [...this.gastos, gasto]
-
-        console.log(this.gastos)
-    }
 }
 
+// En UI no habrá constructor ya que solo servirá para imprimir HTML
 class UI {
     insertarPresupuesto(cantidad) {
-        console.log(cantidad.restante)
-        console.log(cantidad.presupuesto)
-
         const total = document.querySelector('#total');
-        total.textContent = cantidad.presupuesto
-
         const restante = document.querySelector('#restante');
+
+        total.textContent = cantidad.presupuesto;
         restante.textContent = cantidad.restante
-    }
-
-
-    imprimirAlerta(mensaje, tipo) {
-        const div = document.createElement('DIV');
-
-        div.textContent = mensaje;
-        div.classList.add('text-center', 'alert', 'mt-2', 'norepetir')
-
-        if(tipo === 'error') {
-            div.classList.add('alert-danger')
-        } else {
-            div.classList.add('alert-success')
-        }
-
-        
-        const h2 = document.querySelector('h2');
-
-        const norepetir = document.querySelectorAll('.norepetir');
-        if(norepetir.length === 0) {
-            h2.appendChild(div)
-
-            setTimeout(() => {
-                div.remove()
-            }, 2000);
-        }
-        
-
     }
 }
 
-// Instanciar UI
+// Instanciando el presupuesto
+let presupuesto; 
+
+// Instanciando UI
 const ui = new UI();
 
 
-let presupuestoPedido;
+// Funciones
+function preguntarPresupuesto() {
+    const presupuestoUsuario = Number(prompt('¿Cuál es tu presupuesto?'));
+
+    console.log(presupuestoUsuario)
 
 
-// FUNCIONES
-    const pedirPresupuesto = Number(prompt('¿Cuál es tu prespuesto?'));
-
-    // console.log(pedirPresupuesto)
-
-    if(pedirPresupuesto === '' || pedirPresupuesto <= 0 || isNaN(pedirPresupuesto)) {
-        window.location.reload();
-    }
-    
-
-    // Pedimos el presupuesto anteriormente con el prompt y ahora lo asignamos en la clase de Presupuesto
-    presupuestoPedido = new Presupuesto(pedirPresupuesto)
-    console.log(presupuestoPedido)
-
-    // Para hacer que se vea en el html llamamos a la clase de UI junto a su metodo
-    ui.insertarPresupuesto(presupuestoPedido) //presupuestopedido ya que ahí está la cantidad que se pidió;
-
-    
-
-
-function agregarGasto(e) {
-
-    e.preventDefault();
-
-    const nombre = document.querySelector('#gasto').value;
-    const cantidad = document.querySelector('#cantidad').value;
-
-    if(nombre === '' || cantidad === '') {
-        ui.imprimirAlerta('Ambos campos son obligatorios', 'error')
-
-
-        return;
-    } else if(cantidad <= 0 || isNaN(cantidad)) {
-        ui.imprimirAlerta('Cantidad no valida', 'error')
-
-        return
-    } 
-
-
-    // console.log('Añadiendo gastos')
-
-    const gasto = {
-        nombre: nombre,
-        cantidad: cantidad,
-        id: Date.now()
+    if(presupuestoUsuario === 0 || presupuestoUsuario === null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0) {
+        window.location.reload()
     }
 
-    presupuestoPedido.nuevoGasto(gasto)
 
-    
+    // Presupuesto valido
+    presupuesto = new Presupuesto(presupuestoUsuario)
+    // console.log(presupuesto)
+
+    ui.insertarPresupuesto(presupuesto)
+
 }
 
 
+function leerForm(e) {
+    e.preventDefault();
+
+    const nombre = document.querySelector('#leerForm').value;
+    const cantidad = document.querySelector('#cantidad').value;
+}

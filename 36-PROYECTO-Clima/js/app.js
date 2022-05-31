ef21ef31cdaa639a890312fee5ecfe1cfc29c74d
Mona Lisa <santifranquez@gmail.com>
2022-05-31 14:25:11 -0300

    App de clima terminada

--- 36-PROYECTO-Clima/js/app.js
+++ 36-PROYECTO-Clima/js/app.js
@@ -58,18 +58,84 @@ function consultarAPI(ciudad, pais) {
 
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${apiKEY}`;
 
+    spinner(); // muestra un spinner de carga
+
     fetch(url).then((respuesta) => {
         return respuesta.json()
     }).then((datos) => {
+        limpiarHTML()
         // comprobacion, si la ciudad no existe se usará el siguiente mensjae
         if(datos.cod === '404') {
             imprimirAlerta('Está ciudad no existe', 'error');
 
             form.reset()
             return;
-        }
+        };
 
-        console.log(datos)
+        mostrarClima(datos)
 
-    })
+        form.reset()
+        
+
+    });
+}
+
+function spinner() {
+    limpiarHTML()
+
+    const div = document.createElement('DIV');
+    div.classList.add('sk-chase', 'mx-auto')
+    div.innerHTML = `
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+    `;
+
+    resultado.appendChild(div)
+    
+}
+
+
+function mostrarClima(datos) {
+    // Destructuring de un objeto que está adentro de otro objeto
+    const {main: {temp, temp_min, temp_max}} = datos;
+
+    const temperaturaACentigrados = parseInt(temp - 273); // ParseInt para que nos de numeros enteros y restado -273 para pasarlo a centigrados
+    const temperaturaMINACentigrados = parseInt(temp_min - 273);
+    const temperaturaMAXACentigrados = parseInt(temp_max - 273);
+
+
+
+    const tempActual = document.createElement('p');
+    tempActual.innerHTML = `${temperaturaACentigrados} &#8451`;
+    tempActual.classList.add('font-bold', 'text-6xl');
+
+    const tempMin = document.createElement('p');
+    tempMin.innerHTML = `
+        Temp min: ${temperaturaMINACentigrados} &#8451
+    `;
+    tempMin.classList.add('text-2xl')
+
+    const tempMax = document.createElement('p');
+    tempMax.innerHTML = `
+        Temp max: ${temperaturaMAXACentigrados} &#8451
+    `;
+    tempMin.classList.add('text-2xl')
+
+
+    const resultadoDiv = document.createElement('DIV');
+    resultadoDiv.classList.add('text-center', 'text-white');
+
+    resultadoDiv.appendChild(tempActual)
+    resultadoDiv.appendChild(tempMax)
+    resultadoDiv.appendChild(tempMin)
+
+    resultado.appendChild(resultadoDiv)
+};
+
+function limpiarHTML() {
+    resultado.innerHTML = ''
 }
